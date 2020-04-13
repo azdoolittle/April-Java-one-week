fff28f00ee32e42ff6adc7c59372242dd11d9afc
azdoolittle <drewdoolittle@gmail.com>
2020-04-13 15:14:01 -0400

    changed print message and rebuilt project

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -5,7 +5,7 @@ public class MyFirstJava {
     public static void main (String[] args)
     {
 
-        System.out.println("Hello Andrew");
+        System.out.println("Hello, Andrew.");
 
     }
 }
